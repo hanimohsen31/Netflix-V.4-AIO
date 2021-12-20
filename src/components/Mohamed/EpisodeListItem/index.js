c0c24cc6e88c8db6b7900149997242294e6892b9
hanimohen31 <hanimohsen3810@yahoo.com>
2021-12-20 15:04:11 +0200

    epsods

--- src/components/Mohamed/EpisodeListItem/index.js
+++ src/components/Mohamed/EpisodeListItem/index.js
@@ -19,16 +19,24 @@ function EpisodeListItem(props) {
   }
 
     return (
-        <div className="row l-item" >
-          <h2>{video.eps_num}</h2>
-          <a onClick={handlepreview}>
-            <img className="col col-md-3 col-lg-2  ep_img m" src={video.image} alt='' />
+        <div className="container l-item" >
+          <div className='row'>
+
+          <div className="col1 col-sm-12 col-md-1 col-lg-1">
+            <h2>{video.eps_num}</h2>
+          </div>
+
+          <a onClick={handlepreview} className='col2 col-sm-12 col-md-4 col-lg-3'>
+            <img style={{width:'150px',height:'150px',objectFit:'cover'}} className="text-align-center" src={video.image} alt='' />
           </a>
-          <div className="col m">
-            <div className="mb">
+
+          <div className="col3 col-sm-12 col-md-7 col-lg-8">
+            <div className="m-md-0">
               <h2 className="ep-title">{video.title}</h2>
             </div>
             <p>{video.description}</p>
+          </div>
+
           </div>
         </div>
     )
